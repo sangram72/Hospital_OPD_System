5c8420563f426abe2c82ea4d36ceb716fce52d99
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2025-03-24 19:25:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -55,8 +55,8 @@ function App() {
           const { latitude, longitude } = position.coords;
 
           // ✅ Allowed Location (Modify these coordinates)
-          const allowedLat = 22.647424;
-          const allowedLon = 88.420442;
+          const allowedLat = 22.647298;
+          const allowedLon = 88.420461;
           const radius = 0.3; // 0.3 km = 300 meters
 
           const distance = getDistance(latitude, longitude, allowedLat, allowedLon);
