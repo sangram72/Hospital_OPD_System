f6d9feb4e9521dd85ddd533b319ce89fe736aa08
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2025-03-24 20:30:29 +0530

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -6,6 +6,7 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useAuth } from "../../AuthContext"; 
 import './Login.css';
 import loadingGif from '../../assets/loading.gif'; 
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 function Login() {
   const navigate = useNavigate();
@@ -13,6 +14,7 @@ function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLogin = async () => {
     if (!email || !password) {
@@ -26,7 +28,6 @@ function Login() {
       const auth = getAuth();
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
-
       const token = await user.getIdToken(); 
 
       login(token, { email: user.email, uid: user.uid });
@@ -43,13 +44,14 @@ function Login() {
 
   return (
     <div className="container">
+      <h2>Staff Login</h2>
+
       {loading && (
         <div className="loading-overlay">
           <img src={loadingGif} alt="Loading..." className="loading-image" />
         </div>
       )}
 
-      <h2>Login</h2>
       <div className="input-wrapper">
         <input
           className="input-field"
@@ -60,14 +62,17 @@ function Login() {
         />
       </div>
 
-      <div className="input-wrapper">
+      <div className="input-wrapper password-wrapper">
         <input
           className="input-field"
-          type="password"
+          type={showPassword ? "text" : "password"}
           placeholder="Enter your password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        <span className="eye-icon" onClick={() => setShowPassword(!showPassword)}>
+          {showPassword ? <FaEyeSlash /> : <FaEye />}
+        </span>
       </div>
 
       <button onClick={handleLogin} disabled={loading}>
